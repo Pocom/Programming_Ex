86d6f63c8c0a345dadf35f37b330432e8656fbba
Pocom <30789199+Pocom@users.noreply.github.com>
2019-10-21 23:08:25 +0800

    commit

--- 322_Coin Change/t_3_dp.py
+++ 322_Coin Change/t_3_dp.py
@@ -3,7 +3,7 @@ class Solution:
         dp = [amount + 1 for _ in range(amount + 1)]
         dp[0] = 0
         for i in range(1, amount + 1):
-            for j in range(len(coins)):
-                if coins[j] <= i:
-                    dp[i] = min(dp[i], dp[i-coins[j]] + 1)
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
         return -1 if dp[amount] > amount else dp[amount]
